2de9f640a0a0bd4e6dd3c87454e6e428a1ee48e5
devopsdeveloper99 <devopsdeveloper99@gmail.com>
2025-09-22 14:42:14 -0400

    ff added

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,4 +1,4 @@
-from src.calculator import add, subtract, multiply
+from src.calculator import add, subtract, multiply, divide
 
 def test_add():
     assert add(2, 3) == 5
@@ -8,3 +8,7 @@ def test_subtract():
 
 def test_multiply():
     assert multiply(2, 3) == 6
+
+def test_divide():
+    assert divide(6, 3) == 2
+    assert divide(5, 2) == 2.5
